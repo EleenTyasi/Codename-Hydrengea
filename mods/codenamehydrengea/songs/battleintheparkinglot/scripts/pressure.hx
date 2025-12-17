db132e068ce2c6ae2a3258c64509659289a1869f
EleenTyasi <106459601+EleenTyasi@users.noreply.github.com>
2025-12-16 23:52:49 -0800

    changed all pressure scripts to have a facking limit; thanks foom

--- mods/codenamehydrengea/songs/battleintheparkinglot/scripts/pressure.hx
+++ mods/codenamehydrengea/songs/battleintheparkinglot/scripts/pressure.hx
@@ -3,7 +3,7 @@ var EggRoll:Int = 0;
 
 var difficulties = ["hardcore"];
 var difficultyPressures = [0.08];
-var missIncrements = [0.12];
+var missIncrements = [0.02];
 
 function getDifficultyIndex():Int {
     return difficulties.indexOf(PlayState.difficulty);
@@ -19,10 +19,17 @@ function onStartCountdown() {
 function onPlayerMiss() {
     if (pressure >= 0.01) {
         var i = getDifficultyIndex();
-        pressure += missIncrements[i];
+    //HARDCORE!  KRUMP ALL Y'ALL
+        if (i == 3 && pressure < 0.65)
+            pressure += missIncrements[i];
+        else if (i == 3 && pressure > 0.65) {
+            pressure = 0.65;
+            trace("Pressure cap hit for this difficulty, ignoring miss...");
+        }
+
     }
 }
 
 function onDadHit() {
-    if (health > 0.65) health -= pressure * 0.55;
+    if (health > 0.45) health -= pressure * 0.45;
 }
--- mods/codenamehydrengea/songs/peppersandpunches/scripts/pressure.hx
+++ mods/codenamehydrengea/songs/peppersandpunches/scripts/pressure.hx
@@ -2,8 +2,8 @@ var pressure:Float = 0.01;
 var EggRoll:Int = 0;
 
 var difficulties = ["easy", "normal", "hard", "hellsider"];
-var difficultyPressures = [0.02, 0.03, 0.02, 0.04];
-var missIncrements = [0.01, 0.02, 0.04, 0.04];
+var difficultyPressures = [0.01, 0.02, 0.03, 0.04];
+var missIncrements = [0.01, 0.01, 0.02, 0.04];
 
 function getDifficultyIndex():Int {
     return difficulties.indexOf(PlayState.difficulty);
@@ -19,10 +19,38 @@ function onStartCountdown() {
 function onPlayerMiss() {
     if (pressure >= 0.01) {
         var i = getDifficultyIndex();
-        pressure += missIncrements[i];
+// yaaawn, its easy mode.
+        if (i == 0 && pressure < 0.1)
+            pressure += missIncrements[i];
+        else if (i == 0 && pressure > 0.1) {
+            pressure = 0.1;
+            trace("Pressure cap hit for this difficulty, ignoring miss...");
+        }
+// Nice, simple Normal Mode. :)
+        if (i == 1 && pressure < 0.2)
+            pressure += missIncrements[i];
+        else if (i == 1 && pressure > 0.2) {
+            pressure = 0.2;
+            trace("Pressure cap hit for this difficulty, ignoring miss...");
+        }
+// This is gonna get rough for you on hard.
+        if (i == 2 && pressure < 0.35)
+            pressure += missIncrements[i];
+        else if (i == 2 && pressure > 0.35) {
+            pressure = 0.35;
+            trace("Pressure cap hit for this difficulty, ignoring miss...");
+        }
+// Oh, no. You poor bastard, you're on hellsider.
+        if (i == 3 && pressure < 0.65)
+            pressure += missIncrements[i];
+        else if (i == 3 && pressure > 0.65) {
+            pressure = 0.65;
+            trace("Pressure cap hit for this difficulty, ignoring miss...");
+        }
+
     }
 }
 
 function onDadHit() {
-    if (health > 0.65) health -= pressure * 0.55;
+    if (health > 0.45) health -= pressure * 0.45;
 }
